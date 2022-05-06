0ddd06fbfe84c33db138a00c1925f54f6ba93cc8
MichelzonMC11 <101410883+MichelzonMC11@users.noreply.github.com>
2022-05-06 10:00:32 -0400

    Add files via upload

---
+++ pruebas/js/validacion.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form");
+var nomb = document.getElementById("nomb");
+var numChip = document.getElementById("chip");
+var mensajeMostar = document.getElementById("mensaje");
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    let mensajeMostar;
+    let entrar = false;
+
+    
+})
